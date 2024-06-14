5523e371c498b2aa5290610341da00c45194822d
vominhman1611 <minhmanadver@gmail.com>
2024-06-14 08:06:16 +0700

    feature: food paging

--- Apis/SWD392_BE.API/Controllers/FoodController.cs
+++ Apis/SWD392_BE.API/Controllers/FoodController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SWD392_BE.Repositories.Entities;
 using SWD392_BE.Repositories.ViewModels.FoodModel;
+using SWD392_BE.Repositories.ViewModels.PageModel;
 using SWD392_BE.Repositories.ViewModels.StoreModel;
 using SWD392_BE.Repositories.ViewModels.UserModel;
 using SWD392_BE.Services.Interfaces;
@@ -19,10 +21,28 @@ namespace SWD392_BE.API.Controllers
         }
 
         [HttpGet("GetFoods")]
-        public async Task<IActionResult> GetListFood(string storeId)
+        public async Task<IActionResult> GetListFood(string storeId, int pageIndex = 1, int pageSize = 10)
         {
-            var result = await _foodService.getListFood(storeId);
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
+            var result = await _foodService.GetListFood(storeId, pageIndex, pageSize);
+
+            if (result.IsSuccess)
+            {
+                // If successful, return the paged result
+                var pagedResult = (PagedResultViewModel<Food>)result.Data;
+                return Ok(new
+                {
+                    TotalItems = pagedResult.TotalItems,
+                    PageNumber = pagedResult.PageNumber,
+                    PageSize = pagedResult.PageSize,
+                    TotalPages = pagedResult.TotalPages,
+                    Items = pagedResult.Items
+                });
+            }
+            else
+            {
+                // If failed, return BadRequest with the error message
+                return BadRequest(result.Message);
+            }
         }
         [HttpGet("filterFood")]
         public async Task<IActionResult> FilterFood([FromQuery] int? cate)
