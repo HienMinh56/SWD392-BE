8dcd7e8a80786e040da89bc6d951d13705ad9743
vominhman1611 <minhmanadver@gmail.com>
2024-06-14 12:43:26 +0700

    man

--- Apis/SWD392_BE.API/Controllers/FoodController.cs
+++ Apis/SWD392_BE.API/Controllers/FoodController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SWD392_BE.Repositories.Entities;
 using SWD392_BE.Repositories.ViewModels.FoodModel;
+using SWD392_BE.Repositories.ViewModels.PageModel;
 using SWD392_BE.Repositories.ViewModels.StoreModel;
+using SWD392_BE.Repositories.ViewModels.UserModel;
 using SWD392_BE.Services.Interfaces;
 using SWD392_BE.Services.Services;
 
@@ -16,14 +19,42 @@ namespace SWD392_BE.API.Controllers
         {
             _foodService = foodService;
         }
-
-        [HttpGet("GetFoods")]
-        public async Task<IActionResult> GetListFood(string storeId)
+        #region Get list foods
+        /// <summary>
+        /// Get list of foods
+        /// </summary>
+        /// <returns>A list of foods</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetListFood(string storeId, int pageIndex = 1, int pageSize = 10)
         {
-            var result = await _foodService.getListFood(storeId);
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
-        }
+            var result = await _foodService.GetListFood(storeId, pageIndex, pageSize);
 
+            if (result.IsSuccess)
+            {
+                // If successful, return the paged result
+                var pagedResult = (PagedResultViewModel<Food>)result.Data;
+                return Ok(new
+                {
+                    TotalItems = pagedResult.TotalItems,
+                    PageNumber = pagedResult.PageNumber,
+                    PageSize = pagedResult.PageSize,
+                    TotalPages = pagedResult.TotalPages,
+                    Items = pagedResult.Items
+                });
+            }
+            else
+            {
+                // If failed, return BadRequest with the error message
+                return BadRequest(result.Message);
+            }
+        }
+        #endregion
+        [HttpGet("filterFood")]
+        public async Task<IActionResult> FilterFood([FromQuery] int? cate)
+        {
+            var foods = await _foodService.FilterFoodsAsync(cate);
+            return Ok(foods);
+        }
         [HttpPost("AddFood")]
         public async Task<IActionResult> AddFood(string storeId, List<List<FoodViewModel>> foodLists)
         {
@@ -36,6 +67,13 @@ namespace SWD392_BE.API.Controllers
         {
             var currentUser = HttpContext.User;
             var result = await _foodService.UpdateFoodAsync(id, model, currentUser);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+        [HttpPatch("deleteFood")]
+        public async Task<IActionResult> DeleteFood([FromBody] DeleteFoodReqModel request)
+        {
+            var currentUser = HttpContext.User;
+            var result = await _foodService.DeleteFood(request, currentUser);
             return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
     }
