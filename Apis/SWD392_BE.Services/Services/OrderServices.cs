ed218660530e172586058aac497fc159b074d022
NguyenDucLinh411 <linhnguyen41103@gmail.com>
2024-07-12 10:59:40 +0700

    OrderByDescending to all list

--- Apis/SWD392_BE.Services/Services/OrderServices.cs
+++ Apis/SWD392_BE.Services/Services/OrderServices.cs
@@ -83,21 +83,23 @@ namespace SWD392_BE.Services.Services
                 }
                 else
                 {
-                    var orderViewModels = await orders.Select(o => new OrderListViewModel
-                    {
-                        OrderId = o.OrderId,
-                        Name = o.User.Name,
-                        SessionId = o.SessionId,
-                        Price = o.Price,
-                        Quantity = o.Quantity,
-                        StoreName = o.Store.Name,
-                        TransationId = o.TransactionId,
-                        Status = o.Status,
-                        CreatedTime = o.CreatedTime,
-                        CreatedDate = o.CreatedDate,
-                        CampusName = o.User.Campus.Name,
-                        AreaName = o.User.Campus.Area.Name
-                    }).ToListAsync();
+                    var orderViewModels = await orders.OrderByDescending(o => o.CreatedDate)
+                                                      .ThenByDescending(o => o.OrderId) 
+                                                      .Select(o => new OrderListViewModel
+                                                      {
+                                                          OrderId = o.OrderId,
+                                                          Name = o.User.Name,
+                                                          SessionId = o.SessionId,
+                                                          Price = o.Price,
+                                                          Quantity = o.Quantity,
+                                                          StoreName = o.Store.Name,
+                                                          TransationId = o.TransactionId,
+                                                          Status = o.Status,
+                                                          CreatedTime = o.CreatedTime,
+                                                          CreatedDate = o.CreatedDate,
+                                                          CampusName = o.User.Campus.Name,
+                                                          AreaName = o.User.Campus.Area.Name
+                                                      }).ToListAsync();
 
                     result.Data = orderViewModels;
                     result.Message = "Success";
@@ -113,6 +115,7 @@ namespace SWD392_BE.Services.Services
             }
             return result;
         }
+
 
         public async Task<ResultModel> getTotalOrderAmount(DateTime startDate, DateTime endDate)
         {
